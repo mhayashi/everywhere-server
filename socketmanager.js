7c286e61c2921caa520854945821788226682b49
Masahiro Hayashi <hayashi.masahiro@gmail.com>
2010-10-12 17:28:44 +0900

    logging

--- socketmanager.js
+++ socketmanager.js
@@ -72,6 +72,7 @@ SocketManager.prototype.receive = function(client, msg){
   //var parsed = JSON.parse(msg);
   var parsed = msg;
   sys.inspect(parsed);
+  console.log(typeof parsed);
   console.log(parsed.msgType);
   console.log(parsed['msgType']);
   console.log(this.methods[parsed.msgType]);
